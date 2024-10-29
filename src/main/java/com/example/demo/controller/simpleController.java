af3d6ede469da92f68e610ebc035609cb951d665
jaydenyen123 <jialinzhen@gmail.com>
2024-10-28 20:21:19 -0400

    change

---
+++ src/main/java/com/example/demo/controller/simpleController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@RestController
+public class simpleController {
+    @GetMapping("/")
+    public String hello() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                .getRequest();
+        return request.getRemoteAddr();
+    }
+}
